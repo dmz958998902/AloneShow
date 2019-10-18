e77f7c076d4ad0daa1a8ceb500548b6214b5c9df
liuzhiqiang <1446944127@qq.com>
2019-10-18 20:26:20 +0800

    lzq合并

--- src/store/modules/discover.js
+++ src/store/modules/discover.js
@@ -1,4 +1,10 @@
 import axios from 'axios'
+import {
+  stat
+} from 'fs'
+import {
+  Toast
+} from 'vant'
 export default {
   namespaced: true,
 
@@ -35,68 +41,198 @@ export default {
     },
     setDiscoverLoveList(state, payload) {
       state.discoverLoveList = payload
-      console.log('我是爱的')
     }
   },
   actions: {
     getDiscoverList({
-      commit
+      commit,
+      state
     }, payload) {
-      axios.get('https://api.mydeershow.com/mobile/app/dicsovery/homeRecommend?source=mobile&cityName=%E5%85%A8%E5%9B%BD&citySituationName=%E5%85%A8%E5%9B%BD&encrypt=T0FGeDE1NzExNDg2NjY2NTFKSXFGcw%3D%3D&citySituationId=-1&vno=3.2.3&pageNum=1&pageSize=10&type=-1').then(response => {
+      Toast.loading({
+        mask: true,
+        duration: 0,
+        message: '加载中'
+      })
+      axios.get('https://api.mydeershow.com/mobile/app/dicsovery/homeRecommend', {
+        params: {
+          source: 'mobile',
+          cityName: '%E5%85%A8%E5%9B%BD',
+          citySituationName: '%E5%85%A8%E5%9B%BD',
+          encrypt: 'T0FGeDE1NzExNDg2NjY2NTFKSXFGcw%3D%3D',
+          citySituationId: -1,
+          vno: '3.2.3',
+          pageNum: payload.pageNum,
+          pageSize: 10,
+          type: -1
+        }
+      }).then(response => {
         // console.log(response.data)
         let result = response.data
         // console.log(result.data)
         if (result.retCode === "0") {
-          commit('setDiscoverList', result.data)
+          // commit('setDiscoverList', result.data)
+          commit('setDiscoverList', state.discoverList.concat(result.data))
+
+          if (payload.callback) {
+            payload.callback()
+          }
+
+          // 控制Toast消失
+          Toast.clear()
         }
       })
     },
     getDiscoverShowList({
-      commit
+      commit,
+      state
     }, payload) {
-      axios.get('https://api.mydeershow.com/mobile/app/dicsovery/homeRecommend?source=mobile&cityName=%E5%85%A8%E5%9B%BD&citySituationName=%E5%85%A8%E5%9B%BD&encrypt=cktLWjE1NzEyMTY2NDM4NjdrTktySg%3D%3D&citySituationId=-1&vno=3.2.3&pageNum=1&pageSize=10&type=1').then(response => {
+      Toast.loading({
+        mask: true,
+        duration: 0,
+        message: '加载中'
+      })
+      axios.get('https://api.mydeershow.com/mobile/app/dicsovery/homeRecommend', {
+        params: {
+          source: 'mobile',
+          cityName: '%E5%85%A8%E5%9B%BD',
+          citySituationName: '%E5%85%A8%E5%9B%BD',
+          encrypt: 'cktLWjE1NzEyMTY2NDM4NjdrTktySg%3D%3D',
+          citySituationId: -1,
+          vno: '3.2.3',
+          pageNum: payload.pageNum,
+          pageSize: 10,
+          type: 1
+        }
+      }).then(response => {
         // console.log(response.data)
         let result = response.data
-        console.log(result.data)
+        // console.log(result.data)
         if (result.retCode === "0") {
-          commit('setDiscoverShowList', result.data)
+          // commit('setDiscoverList', result.data)
+          commit('setDiscoverShowList', state.discoverShowList.concat(result.data))
+
+          if (payload.callback) {
+            payload.callback()
+          }
+
+          // 控制Toast消失
+          Toast.clear()
         }
       })
     },
     getDiscoverMovList({
-      commit
+      commit,
+      state
     }, payload) {
-      axios.get('https://api.mydeershow.com/mobile/app/dicsovery/homeRecommend?source=mobile&cityName=%E5%85%A8%E5%9B%BD&citySituationName=%E5%85%A8%E5%9B%BD&encrypt=eVQ0aTE1NzEyMTkyNTc5NjVoQnhGRQ%3D%3D&citySituationId=-1&vno=3.2.3&pageNum=1&pageSize=10&type=2').then(response => {
+      Toast.loading({
+        mask: true,
+        duration: 0,
+        message: '加载中'
+      })
+      axios.get('https://api.mydeershow.com/mobile/app/dicsovery/homeRecommend', {
+        params: {
+          source: 'mobile',
+          cityName: '%E5%85%A8%E5%9B%BD',
+          citySituationName: '%E5%85%A8%E5%9B%BD',
+          encrypt: 'eVQ0aTE1NzEyMTkyNTc5NjVoQnhGRQ%3D%3D',
+          citySituationId: -1,
+          vno: '3.2.3',
+          pageNum: payload.pageNum,
+          pageSize: 10,
+          type: 2
+        }
+      }).then(response => {
         // console.log(response.data)
         let result = response.data
-        console.log(result.data)
+        // console.log(result.data)
         if (result.retCode === "0") {
-          commit('setDiscoverMovList', result.data)
+          // commit('setDiscoverList', result.data)
+          commit('setDiscoverMovList', state.discoverMovList.concat(result.data))
+
+          if (payload.callback) {
+            payload.callback()
+          }
+
+          // 控制Toast消失
+          Toast.clear()
         }
       })
     },
     getDiscoverSpotList({
-      commit
+      commit,
+      state
     }, payload) {
-      axios.get('https://api.mydeershow.com/mobile/app/dicsovery/homeRecommend?source=mobile&cityName=%E5%85%A8%E5%9B%BD&citySituationName=%E5%85%A8%E5%9B%BD&encrypt=emNNWTE1NzEyMzIxNjQxODhDSHV1SQ%3D%3D&citySituationId=-1&accessToken=D31EF6850017B2D0DED2AAC76848636A&vno=3.2.3&pageNum=1&pageSize=10&type=3').then(response => {
+      Toast.loading({
+        mask: true,
+        duration: 0,
+        message: '加载中'
+      })
+      axios.get('https://api.mydeershow.com/mobile/app/dicsovery/homeRecommend', {
+        params: {
+          source: 'mobile',
+          cityName: '%E5%85%A8%E5%9B%BD',
+          citySituationName: '%E5%85%A8%E5%9B%BD',
+          encrypt: 'emNNWTE1NzEyMzIxNjQxODhDSHV1SQ%3D%3D',
+          citySituationId: -1,
+          vno: '3.2.3',
+          pageNum: payload.pageNum,
+          pageSize: 10,
+          type: 3
+        }
+      }).then(response => {
         // console.log(response.data)
         let result = response.data
-        console.log(result.data)
+        // console.log(result.data)
         if (result.retCode === "0") {
-          commit('setDiscoverSpotList', result.data)
+          // commit('setDiscoverList', result.data)
+          commit('setDiscoverSpotList', state.discoverSpotList.concat(result.data))
+
+          if (payload.callback) {
+            payload.callback()
+          }
+
+          // 控制Toast消失
+          Toast.clear()
         }
       })
     },
     getDiscoverLoveList({
-      commit
+      commit,
+      state
     }, payload) {
-      axios.get('https://api.mydeershow.com/mobile/app/aboutchat/repo/findAboutchatRepoList?source=mobile&cityName=%E5%85%A8%E5%9B%BD&citySituationName=%E5%85%A8%E5%9B%BD&encrypt=UG5FTTE1NzEyOTc1MTQ0MzNFZGR6OA%3D%3D&citySituationId=-1&vno=3.2.3&pageNum=1&pageSize=10&type=6').then(response => {
+      Toast.loading({
+        mask: true,
+        duration: 0,
+        message: '加载中'
+      })
+      axios.get('https://api.mydeershow.com/mobile/app/aboutchat/repo/findAboutchatRepoList', {
+        params: {
+          source: 'mobile',
+          cityName: '%E5%85%A8%E5%9B%BD',
+          citySituationName: '%E5%85%A8%E5%9B%BD',
+          encrypt: 'UG5FTTE1NzEyOTc1MTQ0MzNFZGR6OA',
+          citySituationId: -1,
+          vno: '3.2.3',
+          pageNum: payload.pageNum,
+          pageSize: 10,
+          type: 6
+        }
+      }).then(response => {
+        // console.log(response.data)
         let result = response.data
-        if (result.retCode === '0') {
-          commit('setDiscoverLoveList', result.data)
+        // console.log(result.data)
+        if (result.retCode === "0") {
+          // commit('setDiscoverList', result.data)
+          commit('setDiscoverLoveList', state.discoverLoveList.concat(result.data))
+
+          if (payload.callback) {
+            payload.callback()
+          }
+
+          // 控制Toast消失
+          Toast.clear()
         }
       })
     }
-
   }
 }
